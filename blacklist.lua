3e44eecbfc5fefba3ce972b19eed7a06463139f2
7rab <119785971+lmaooa1043@users.noreply.github.com>
2024-04-30 20:21:03 +0300

    Update blacklist.lua

--- blacklist.lua
+++ blacklist.lua
@@ -1,11 +1,9 @@
-local BlacklistData = {}
-
-BlacklistData.blacklistedIDs = {
-    [4069206905] = true,
+return {
+    blacklistedIDs = {
+        [4069206905] = true,
+    },
+    blacklistedUsernames = {
+        ["King123coolest1"] = true,
+        ["T_azb"] = true,
+    }
 }
-
-BlacklistData.blacklistedUsernames = {
-    ["King123coolest1"] = true,
-}
-
-return BlacklistData
