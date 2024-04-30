4f87654a47276cee086feea612434e8e084cf4d6
7rab <119785971+lmaooa1043@users.noreply.github.com>
2024-04-30 20:43:43 +0300

    Update blacklist.lua

--- blacklist.lua
+++ blacklist.lua
@@ -3,12 +3,10 @@ return {
         [4069206905] = true, -- KingWay
         [1897359984] = true, -- PoweredWay
         [1972686697] = true, --Africinous
-        [4785601821] = true, --0mg Testing
     },
     blacklistedUsernames = {
         ["King123coolest1"] = true, -- KingWay, Hacked into a nonexistant database.
         ["PoweredWay"] = true, -- PoweredWay, Told me i wouldnt make it
         ["5DollarP"] = true, -- Africinous, Saying that me and king should make out
-        ["RyosUser"] = true, --0mg Testing
     }
 }
