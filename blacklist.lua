b28e383ba668b2d97b006e7942d7b04e1200e94f
7rab <119785971+lmaooa1043@users.noreply.github.com>
2024-05-01 04:02:35 +0300

    Update blacklist.lua

--- blacklist.lua
+++ blacklist.lua
@@ -1,16 +1,51 @@
+--[[
+
+ShizoHub Blacklist
+
+Blacklisted UserIDS and Usernames,
+Dont think your a skidlord supreme by leaking this, if i really wanted to hide it you wouldnt find it anywhere
+
+For retards, PB = Perma-Blacklisted
+
+]]
+
 return {
     blacklistedIDs = {
-        [4069206905] = true, -- Perma-Blacklist, KingWay
-        [1897359984] = true, -- Perma-Blacklist, PoweredWay
-        [1972686697] = true, -- Perma-Blacklist, Afric
-        [193652114] = true, -- Perma-Blacklist, Mystic
-        [1672135289] = true, -- Perma-Blacklist, RestrictedWay
+        [1] = true, -- PB, Roblox
+        [4069206905] = true, -- PB, KingWay
+        [1897359984] = true, -- PB, PoweredWay
+        [1972686697] = true, -- PB, Afric
+        [193652114] = true, -- PB, Mystic
+        [1672135289] = true, -- PBt, RestrictedWay
+        [1653161605] = true, -- PB, RestrictedWay
+        [3742702895] = true, -- PB, Powered's Alt
+        [5816956713] = true, -- PB, Powered's Alt
+        [5785414856] = true, -- PB, Powered's Alt
+        [3741761673] = true, -- PB, Tixcyy Stand Creator
+        [3639250432] = true, -- PB, Tixcyy Stand Creator Alt
+        [4624493186] = true, -- PB, Tixcyy Stand Creator Alt
+        [348791490] = true, -- PB, Tixcyy Stand Co-Creator ( IUX )
+        [687968198] = true, -- PB, Tixcyy Stand Co-Creator Alt ( IUX )
+        [4758490406] = true, -- PB, Tixcyy Stand Co-Creator ( Seth )
+        [2476532246] = true, -- PB, ObeseKid
     },
     blacklistedUsernames = {
-        ["King123coolest1"] = true, -- Perma-Blacklist, KingWay, Hacked into a nonexistant database.
-        ["PoweredWay"] = true, -- Perma-Blacklist, PoweredWay, Told me i wouldnt make it
-        ["5DollarP"] = true, -- Perma-Blacklist, Afric, Saying that me and king should make out
-        ["1xMystic"] = true, -- Perma-Blacklist, Mystic
-        ["k1rby92"] = true, -- Perma-Blacklist, RestrictedWay
+        ["Roblox"] = true, -- PB, Roblox
+        ["King123coolest1"] = true, -- PB, KingWay
+        ["PoweredWay"] = true, -- PB, PoweredWay
+        ["5DollarP"] = true, -- PB, Afric
+        ["1xMystic"] = true, -- PB, Mystic
+        ["k1rby92"] = true, -- PB, RestrictedWay
+        ["kevinedwerdsjg"] = true, -- PB, ElectricWay
+        ["PoweredWay_ALT"] = true, -- PB, Powered's Alts
+        ["PoweredsWay_ALT"] = true, -- PB, Powered's Alts
+        ["PoweredWays_ALT"] = true, -- PB, Powered's Alts
+        ["XK5NG"] = true, -- PB, Tixcyy Stand Creator
+        ["4K7NZ"] = true, -- PB, Tixcyy Stand Creator Alt
+        ["Ticxyylolz"] = true, -- PB, Tixcyy Stand Creator Alt
+        ["ClientSidee"] = true, -- PB, Tixcyy Stand Co-Creator ( IUX )
+        ["lIIIIlIIllllllllIIl"] = true, -- PB, Tixcyy Stand Co-Creator Alt ( IUX )
+        ["hatredframe"] = true, -- PB, Tixcyy Stand Co-Creator ( SETH )
+        ["obese2drippy"] = true, -- PB, ObeseKid   
     }
 }
