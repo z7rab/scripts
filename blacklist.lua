20db7ff16ece1bc6455a0caef6fa11f0c49d6478
7rab <119785971+lmaooa1043@users.noreply.github.com>
2024-05-01 03:42:22 +0300

    Update blacklist.lua

--- blacklist.lua
+++ blacklist.lua
@@ -4,11 +4,13 @@ return {
         [1897359984] = true, -- Perma-Blacklist, PoweredWay
         [1972686697] = true, -- Perma-Blacklist, Afric
         [193652114] = true, -- Perma-Blacklist, Mystic
+        [1672135289] = true, -- Perma-Blacklist, RestrictedWay
     },
     blacklistedUsernames = {
-        ["King123coolest1"] = true,-- Perma-Blacklist, KingWay, Hacked into a nonexistant database.
+        ["King123coolest1"] = true, -- Perma-Blacklist, KingWay, Hacked into a nonexistant database.
         ["PoweredWay"] = true, -- Perma-Blacklist, PoweredWay, Told me i wouldnt make it
         ["5DollarP"] = true, -- Perma-Blacklist, Afric, Saying that me and king should make out
         ["1xMystic"] = true, -- Perma-Blacklist, Mystic
+        ["k1rby92"] = true, -- Perma-Blacklist, RestrictedWay
     }
 }
