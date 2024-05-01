15d0bf6891c57f64563071656adcecb8fed43119
7rab <octocat@github.com>
2024-05-01 03:39:54 +0300

    a new commit message

--- blacklist.lua
+++ blacklist.lua
@@ -1,1 +1,14 @@
-nigga
+return {
+    blacklistedIDs = {
+        [4069206905] = true, -- Perma-Blacklist, KingWay
+        [1897359984] = true, -- Perma-Blacklist, PoweredWay
+        [1972686697] = true, -- Perma-Blacklist, Afric
+        [193652114] = true, -- Perma-Blacklist, Mystic
+    },
+    blacklistedUsernames = {
+        ["King123coolest1"] = true,-- Perma-Blacklist, KingWay, Hacked into a nonexistant database.
+        ["PoweredWay"] = true, -- Perma-Blacklist, PoweredWay, Told me i wouldnt make it
+        ["5DollarP"] = true, -- Perma-Blacklist, Afric, Saying that me and king should make out
+        ["1xMystic"] = true, -- Perma-Blacklist, Mystic
+    }
+}
